a04389b7481f863306f73b7016c4314f188774bb
baekDongJae <pandora2948@naver.com>
2022-03-27 00:53:15 +0900

    otp 알고리즘 구현

---
+++ scripts/app.js
@@ -0,0 +1,401 @@
+const keyHanddler = {
+
+  createAlphabetList(cb) {
+    const list = [];
+    for (let i = 0; i < asciiCodeAssets.alphabetLength * 3; i += 1) {
+      const alphabet = document.createElement('li');
+      if(i === 0) {
+        alphabet.setAttribute('id', 'firstKey');
+      }
+      alphabet.innerHTML = cb(i);
+      alphabet.classList.add('scroll');
+      list.push(alphabet);
+    }
+    return list;
+  },
+}
+
+const scrollEvent = {
+  scrollLeft: 0,
+  clickCoordinate: 0,
+  scrollValue: 0,
+  listWidth: 0,
+  tmpKey:0,
+
+  handleKey() {
+    generatePadKeys();
+    const keyIndicator = domAssets.keyIndicatorList;
+    const {keyArray} = translatorAssets;
+    if (translatorAssets.algorithm === 'oneTimePad') {
+      
+    }
+    else {
+      keyIndicator.innerHTML = '';
+      keyArray.forEach((k) => {
+        keyIndicator.innerHTML += `<li class="key-indicator">${k}</li>`
+      })
+      algorithm[translatorAssets.algorithm]();
+    }
+  },
+
+  handlePosition() {
+    const scroll = domAssets.keyScroll
+    scroll.childNodes.forEach((li) => {
+      li.style.transform = `translateX(-${this.scrollValue}px)`
+      
+      if (this.scrollValue > scroll.offsetWidth) {
+        setTimeout(() => {
+          li.classList.remove('scroll');
+          li.style.transform = `translateX(-${this.scrollValue % scroll.offsetWidth}px)`
+        }, 250)
+        setTimeout(() => {
+          li.classList.add('scroll');
+        }, 260)
+      }
+    });
+  },
+
+  handleClick(e) {
+    const presentSelctcor = document.querySelector('#firstKey')
+    const scroll = domAssets.keyScroll
+    if(!presentSelctcor === false) {
+      presentSelctcor.removeAttribute('id')
+    }
+    this.scrollLeft = scroll.offsetLeft;
+    this.clickCoordinate = e.target.offsetLeft;
+    this.scrollValue = (this.clickCoordinate - this.scrollLeft);
+    this.listWidth = scroll.children[0].offsetWidth;
+    
+    this.handlePosition();
+    this.tmpKey = Math.floor(this.scrollValue / this.listWidth % asciiCodeAssets.alphabetLength);
+    for (let i = 0; i <= asciiCodeAssets.alphabetLength; i++) {
+      if (!this.tmpKey) {
+        scroll.children[26].setAttribute('id', 'firstKey');
+      }
+      else if (this.scrollLeft + this.listWidth * this.tmpKey <= scroll.children[i].offsetLeft && this.scrollLeft + this.listWidth * (this.tmpKey + 0.5) >= scroll.children[i].offsetLeft) {
+        scroll.children[i].setAttribute('id', 'firstKey');
+      }
+    }
+  }
+}
+
+const algorithm = {
+  caesar() {
+    const {spaceCode, symbolStartCode, symbolLastCode, numberStartCode, numberLastCode, upperStartCode, upperLastCode, lowerStartCode, lowerLastCode, symbolLength, numberLength, alphabetLength} = asciiCodeAssets;
+    const {sourceInput, modifiedOutput} = domAssets;
+    const isEncrypter = translatorAssets.isEncrypter;
+    const listOfAscii = [];
+    const inputLength = sourceInput.value.length;
+    const asciiLength = listOfAscii.length;
+    for (let i = 0; i < inputLength; i += 1) {
+      const asciiCode = sourceInput.value.charCodeAt(i);
+      const key = translatorAssets.keyArray[0];
+      if (asciiCode === spaceCode) {
+        listOfAscii[i] = asciiCode;
+      }
+
+      else if (asciiCode <= symbolLastCode && asciiCode >= symbolStartCode) {
+        if (isEncrypter) {
+          listOfAscii[i] = (asciiCode % symbolStartCode + key) % symbolLength;
+          listOfAscii[i] += symbolStartCode;
+        }
+        else {
+          listOfAscii[i] = (asciiCode % symbolStartCode % symbolLength - key)
+
+          if (listOfAscii[i] < 0) {
+            listOfAscii[i] = symbolLength + listOfAscii[i];
+          }
+          listOfAscii[i] += symbolStartCode;
+        }
+      }
+
+      else if (asciiCode <= numberLastCode && asciiCode >= numberStartCode) {
+        if (isEncrypter) {
+          listOfAscii[i] = (asciiCode % numberStartCode + key) % numberLength;
+          listOfAscii[i] += numberStartCode;
+        }
+        else {
+          listOfAscii[i] = (asciiCode % numberStartCode % numberLength - key)
+
+          if (listOfAscii[i] < 0) {
+            listOfAscii[i] = numberLength + listOfAscii[i];
+          }
+          listOfAscii[i] += numberStartCode;
+        }
+      }
+
+      else if (asciiCode <= upperLastCode && asciiCode >= upperStartCode) {
+        if(isEncrypter) {
+          listOfAscii[i] = (asciiCode % upperStartCode + key) % alphabetLength;
+          listOfAscii[i] += upperStartCode;
+        }
+        else {
+          listOfAscii[i] = (asciiCode % upperStartCode % alphabetLength - key)
+          if (listOfAscii[i] < 0) {
+            listOfAscii[i] = alphabetLength + listOfAscii[i];
+          }
+          listOfAscii[i] += upperStartCode;
+        }
+      }
+
+      else if (asciiCode <= lowerLastCode && asciiCode >= lowerStartCode) {
+        if (isEncrypter) {
+          listOfAscii[i] = (asciiCode % lowerStartCode + key) % alphabetLength;
+          listOfAscii[i] += lowerStartCode;
+        }
+
+        else {
+          listOfAscii[i] = (asciiCode % lowerStartCode % alphabetLength - key);
+          if (listOfAscii[i] < 0) {
+            listOfAscii[i] = alphabetLength + listOfAscii[i];
+          }
+          listOfAscii[i] += lowerStartCode;
+        }
+      }
+      else {
+        listOfAscii[i] = asciiCode;
+      }
+    }
+    if (inputLength < asciiLength) {
+      for (let i = 0; i < asciiLength - inputLength; i += 1) {
+        listOfAscii.pop();
+      }
+    }
+    const tag = listOfAscii
+    .map((curr) => {
+      return `&#${curr};`;
+    })
+    .reduce((acc, curr) => {
+      return acc + curr;
+    }, '')
+    modifiedOutput.innerHTML = tag;
+  },
+
+  vigenere() {
+    const {upperStartCode, upperLastCode, lowerStartCode, lowerLastCode, alphabetLength} = asciiCodeAssets;
+    const {sourceInput, modifiedOutput} = domAssets;
+    const isEncrypter = translatorAssets.isEncrypter;
+    const listOfAscii = [];
+    const inputLength = sourceInput.value.length;
+    const asciiLength = listOfAscii.length;
+
+    for (let i = 0; i < inputLength; i += 1) {
+      const asciiCode = sourceInput.value.charCodeAt(i);
+      const key = translatorAssets.keyArray[i % translatorAssets.keyArray.length];
+
+      if (asciiCode <= upperLastCode && asciiCode >= upperStartCode) {
+        if(isEncrypter) {
+          listOfAscii[i] = (asciiCode % upperStartCode + key) % alphabetLength;
+          listOfAscii[i] += upperStartCode;
+        }
+
+        else {
+          listOfAscii[i] = (asciiCode % upperStartCode % alphabetLength - key)
+          if (listOfAscii[i] < 0) {
+            listOfAscii[i] = alphabetLength + listOfAscii[i];
+          }
+          listOfAscii[i] += upperStartCode;
+        }
+      }
+
+      else if (asciiCode <= lowerLastCode && asciiCode >= lowerStartCode) {
+        if (isEncrypter) {
+          listOfAscii[i] = (asciiCode % lowerStartCode + key) % alphabetLength;
+          listOfAscii[i] += lowerStartCode;
+        }
+
+        else {
+          listOfAscii[i] = (asciiCode % lowerStartCode % alphabetLength - key);
+          if (listOfAscii[i] < 0) {
+            listOfAscii[i] = alphabetLength + listOfAscii[i];
+          }
+          listOfAscii[i] += lowerStartCode;
+        }
+      }
+
+      else {
+        listOfAscii[i] = asciiCode;
+      }
+    }
+    if (inputLength < asciiLength) {
+      for (let i = 0; i < asciiLength - inputLength; i += 1) {
+        listOfAscii.pop();
+      }
+    }
+
+    const tag = listOfAscii
+    .map((curr) => {
+      return `&#${curr};`;
+    })
+    .reduce((acc, curr) => {
+      return acc + curr;
+    }, '')
+    modifiedOutput.innerHTML = tag;
+  },
+
+  oneTimePad() {
+    const {sourceInput, modifiedOutput} = domAssets;
+    const isEncrypter = translatorAssets.isEncrypter;
+    const listOfAscii = [];
+    if (isEncrypter) {
+      const inputLength = sourceInput.value.length;
+      for (let i = 0; i < inputLength; i += 1) {
+        const asciiCode = sourceInput.value.charCodeAt(i);
+        const key = translatorAssets.keyArray[i];
+        listOfAscii[i] = (asciiCode ^ key).toString(2);
+        }
+        const tag = listOfAscii
+        .map((curr) => {
+          return `${curr.padStart(8, '0')} `;
+        })
+        .reduce((acc, curr) => {
+          return acc + curr;
+        }, '').trim();
+        modifiedOutput.innerText = tag;
+      }
+    else{
+      const inputLength = sourceInput.value.split(' ').length;
+      for(let i = 0; i < inputLength; i += 1) {
+        const asciiCode = parseInt(sourceInput.value.split(' ')[i], 2);
+        const key = translatorAssets.keyArray[i];
+        listOfAscii[i] = asciiCode ^ key;
+      }
+
+      const tag = listOfAscii
+      .map((curr) => {
+        return `&#${curr};`;
+      })
+      .reduce((acc, curr) => {
+        return acc + curr;
+      }, '')
+      modifiedOutput.innerHTML = tag;
+    }
+  }
+}
+
+const handleDisplay = (t) => {
+    if (t.classList.contains('hidden')) {
+      t.removeAttribute('display') 
+    }
+    else {
+      t.removeAttribute('display')
+    }
+}
+
+
+const handleheaderPopup = (toggle) => {
+  const {algorithmSelectorArrow, algorithmPopup} = domAssets;
+  if (toggle) {
+    algorithmSelectorArrow.classList.toggle('selected');
+    algorithmPopup.classList.toggle('hidden');
+  }
+  else {
+    algorithmSelectorArrow.classList.remove('selected');
+    algorithmPopup.classList.remove('visible');
+    algorithmPopup.classList.add('hidden') ;
+  }
+}
+
+const handleAlgorithm = (target) => {
+  const presentAlgorithm = translatorAssets.algorithm;
+  const selectedAlgorithm = target.getAttribute('value');
+  if (presentAlgorithm === selectedAlgorithm)  return;
+  translatorAssets.algorithm = selectedAlgorithm;
+  translatorAssets.keyArray = [0];
+  domAssets.algorithmDisplay.innerText = target.innerText;
+  domAssets.algorithmSelector.childNodes[0].nodeValue = target.innerText;
+  scrollEvent.handleKey();
+}
+
+const handleEncrypter = () => {
+  const input = domAssets.sourceInput;
+  const output = domAssets.modifiedOutput;
+  const buffer = input.value;
+  input.value = output.innerHTML;
+  output.innerText = buffer;
+  if (translatorAssets.isEncrypter) {
+    domAssets.encrypterExchangerImage.classList.add('decrypter');
+    translatorAssets.isEncrypter = false;
+    algorithm[translatorAssets.algorithm]();
+  }
+  else {
+    domAssets.encrypterExchangerImage.classList.remove('decrypter');
+    translatorAssets.isEncrypter = true;
+    algorithm[translatorAssets.algorithm]();
+  }
+}
+
+const handleKeyWrapper = (toggle, isButton) => {
+  if (toggle) {
+    if(isButton) {
+      domAssets.keyPopup.classList.toggle('hidden');
+    }
+    else {
+      domAssets.keyPopup.classList.remove('hidden');
+    }
+  }
+  else {
+    domAssets.keyPopup.classList.add('hidden');
+  }
+}
+
+const handleCopy = (target) => {
+  if (target === domAssets.sourceCopyButton || target === domAssets.sourceCopyButtonImage) {
+    navigator.clipboard.writeText(domAssets.sourceInput.value);
+  }
+  else {
+    navigator.clipboard.writeText(domAssets.modifiedOutput.innerHTML);
+  }
+}
+
+const removeText = () => {
+  domAssets.sourceInput.value = '';
+  domAssets.modifiedOutput.innerText = '';
+}
+
+const selectKey = () => {
+  if(translatorAssets.keyArray.length > 5) return;
+  if (translatorAssets.algorithm === 'caesar' || translatorAssets.keyArray[0] === 0) {
+    translatorAssets.keyArray[0] = scrollEvent.tmpKey;
+  }
+  else {
+    domAssets.beforeKeyIndicator.innerText = 'Keys'
+    translatorAssets.keyArray.push(scrollEvent.tmpKey);
+  }
+  scrollEvent.handleKey()
+}
+
+const generatePadKeys = () => {
+  if(translatorAssets.algorithm !== 'oneTimePad') return;
+  if(translatorAssets.isEncrypter) {
+    const inputLength = domAssets.sourceInput.value.length;
+    translatorAssets.keyArray = [];
+    for(let i = 0; i < inputLength; i++) {
+      translatorAssets.keyArray.push(Math.ceil(Math.random() * 256));
+    }
+    domAssets.keyInput.value = translatorAssets.keyArray;
+  }
+  else {
+    translatorAssets.keyArray = [];
+    for (let i = 0; i < domAssets.keyInput.value.split(',').length; i += 1){
+      translatorAssets.keyArray.push(domAssets.keyInput.value.split(',')[i]);
+    }
+  }
+  algorithm.oneTimePad();
+}
+
+handleKeyRole = () => {
+  const keyInput = document.querySelector('#keyInput');
+  const footerKey = document.querySelector('#footerKeyDisplay');
+  if(translatorAssets.algorithm === 'oneTimePad') {
+    translator.keySelector.classList.add('hidden');
+    keyInput.classList.remove('hidden');
+    footerKey.classList.add('hidden');
+  }
+  else {
+    translator.keySelector.classList.remove('hidden');
+    keyInput.classList.add('hidden');
+    footerKey.classList.remove('hidden');
+  }
+  
+}
